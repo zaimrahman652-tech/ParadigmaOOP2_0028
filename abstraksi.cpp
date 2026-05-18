18494f73d382abf8e705fcbbd4535c99dd81492c
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-18 12:49:38 +0700

    isi fungsi di class

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -6,3 +6,16 @@ private:
     // Atribut privat yang disembunyikan dari luar kelas
     string x, y;
 
+public:
+    // Method untuk mengisi nilai ke dalam private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    // Method untuk menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
