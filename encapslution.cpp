d6d38c78c244efe25f8ee842a422ba5c1d147cfd
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-18 12:45:49 +0700

    isi fungsi kelas

--- encapslution.cpp
+++ encapslution.cpp
@@ -5,3 +5,16 @@ class remoteLampu {
 private:
     // Variabel array untuk menyimpan nama saklar (disembunyikan dari luar kelas)
     string saklarNo[10];
+
+public:
+    // Setter: Metode untuk mengubah nilai saklarNo berdasarkan indeks
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+
+    // Getter: Metode untuk mengambil nilai saklarNo berdasarkan indeks
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
