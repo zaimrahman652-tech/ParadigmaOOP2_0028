0cb660385ef52922bc86479077c6dbde2a8a115e
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-18 12:52:42 +0700

    bikin class base final

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,2 +1,11 @@
  #include <iostream>
 using namespace std;
+
+// Class yang menggunakan kata kunci 'final'
+// Class ini tidak akan bisa diturunkan/diwariskan ke class lain
+class baseClass final {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class" << endl;
+    }
+};
