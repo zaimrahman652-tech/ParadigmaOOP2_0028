e34f51b9d87fb7144eff9d7a9a97e46be82252b6
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-18 12:39:19 +0700

    tambah kelas abstrak

--- polyrmo.cpp
+++ polyrmo.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// Kelas induk (Base Class) yang bersifat abstrak
+class seseorang {
+public:
+    // Pure virtual function membuat kelas ini menjadi abstract class
+    virtual void pesan() = 0; 
+    
