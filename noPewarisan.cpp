854fb75dcbc47098e510f0c6c0e5cc8ba38a2638
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-18 12:54:37 +0700

    bikin class base turunan

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -18,3 +18,10 @@ public:
     }
 };
 
+// Derived Class yang sukses mewarisi baseClass1
+class derivedClass : public baseClass1 {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class" << endl;
+    }
+};
