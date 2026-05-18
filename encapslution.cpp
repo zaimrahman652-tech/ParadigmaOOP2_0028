0fa30d10fae3b9d5ab5606358f94c12fd75274a8
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-18 12:44:49 +0700

    add include dan kelas

--- encapslution.cpp
+++ encapslution.cpp
@@ -5,34 +5,3 @@ class remoteLampu {
 private:
     // Variabel array untuk menyimpan nama saklar (disembunyikan dari luar kelas)
     string saklarNo[10];
-
-public:
-    // Setter: Metode untuk mengubah nilai saklarNo berdasarkan indeks
-    void setSaklarNo(int i, string value) {
-        saklarNo[i] = value;
-    }
-
-    // Getter: Metode untuk mengambil nilai saklarNo berdasarkan indeks
-    string getSaklarNo(int i) {
-        return saklarNo[i];
-    }
-};
-
-int main() {
-    // Membuat objek dari kelas remoteLampu
-    remoteLampu lampuRumah;
-
-    // Mengisi data saklar menggunakan metode Setter (setSaklarNo)
-    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
-    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
-    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
-    lampuRumah.setSaklarNo(3, "Lampu Dapur");
-
-    // Menampilkan data saklar menggunakan metode Getter (getSaklarNo)
-    cout << lampuRumah.getSaklarNo(0) << endl;
-    cout << lampuRumah.getSaklarNo(1) << endl;
-    cout << lampuRumah.getSaklarNo(2) << endl;
-    cout << lampuRumah.getSaklarNo(3) << endl;
-
-    return 0;
-}
