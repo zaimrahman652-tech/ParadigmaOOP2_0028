37f97e81de2d0c3173cfd70ad8dd8965b0fffbcf
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-05-18 12:37:59 +0700

    Add initial implementation of C++ files for OOP concepts

---
+++ abstraksi.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private:
+    // Atribut privat yang disembunyikan dari luar kelas
+    string x, y;
+
+public:
+    // Method untuk mengisi nilai ke dalam private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    // Method untuk menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
+int main() {
+    // Membuat objek dari kelas AbstraksiKlas
+    AbstraksiKlas ak;
+
+    // Mengisi data secara tidak langsung melalui method setXY
+    ak.setXY("Yogyakarta", "Kampus");
+
+    // Menampilkan data menggunakan method display
+    ak.display();
+
+    return 0;
+}
---
+++ no0verriding.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    // Menggunakan kata kunci 'final' pada virtual function
+    // Fungsi ini tidak boleh di-override (ditulis ulang) oleh class turunan
+    virtual void perkenalan() final {
+        cout << "Hallo saya Function dari base class" << endl;
+    }
+};
+
+class derivedClass : public baseClass {
+public:
+    // Dosen Anda mengubah nama fungsinya menjadi 'perkenalan1' (ada angka 1)
+    // Ini dilakukan agar kode tidak eror saat di-compile!
+    void perkenalan1() {
+        cout << "Hallo saya function dari derived Class" << endl;
+    }
+};
+
+int main() {
+    // Membuat objek dari derivedClass
+    derivedClass a;
+    
+    // Memanggil fungsi perkenalan1 milik derivedClass
+    a.perkenalan1();
+
+    return 0;
+}
